7477ee881237b16c54a8daa5c2e88bfbe22e0fc3
Nathan Burgess <nathans.email.address@gmail.com>
2022-11-17 16:22:17 -0800

    GET /api/v1/secrets route returns all secrets

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,14 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS secrets;
+
+CREATE TABLE secrets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR,
+    description VARCHAR,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO secrets (title, description)
+VALUES ('President Secret', 'The President slept with a hooker. Don''t tell anyone');
+
