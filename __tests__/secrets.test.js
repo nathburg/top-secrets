4a9f52081d19f0dc82384aa3f8f8d68dbe9113e9
Nathan Burgess <nathans.email.address@gmail.com>
2022-11-17 17:27:26 -0800

    POST /secrets adds new secret to db

--- __tests__/secrets.test.js
+++ __tests__/secrets.test.js
@@ -7,13 +7,24 @@ describe('secrets routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  test('POST /api/v1/secrets posts new secret', async () => {
+    const res = await request(app)
+      .post('/api/v1/secrets')
+      .send({ title: '418', description: 'The President is a teapot.' });
+    console.log(res.body);
+    expect(res.status).toEqual(200);
+    expect(res.body.description).toBe('The President is a teapot.');
+  });
+
   test('GET /api/v1/secrets returns all secrets', async () => {
     const res = await request(app).get('/api/v1/secrets');
-    console.log(res.body[0]);
+    expect(res.status).toBe(200);
     expect(res.body[0].description).toBe(
       "The President slept with a hooker. Don't tell anyone"
     );
   });
+
   afterAll(() => {
     pool.end();
   });
